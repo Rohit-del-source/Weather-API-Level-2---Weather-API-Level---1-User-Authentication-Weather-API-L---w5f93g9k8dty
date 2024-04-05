2f5329a32181eaab0284438a28bba1a94b969d21
Rohit-del-source <rohitverma18158920@gmail.com>
2024-04-05 17:56:57 +0530

    initial

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,19 +1,47 @@
 const WeatherData = require('../models/weatherDataModel');
-
+const fs = require("fs");
 const getWeatherByCityName = async (req, res) => {
-  const cityName = req.params.cityName;
+const cityName = req.params.cityName;
+try {
+// TODO: Implement logic to retrieve weather data by city name
+// Example response when data is found:
+// res.status(200).json({ weatherData });
+// Example response when data is not found:
+// res.status(404).json({ message: 'Weather data not found for the given city' });
+const cityData = await WeatherData.findOne({ cityName });
+if (cityData) {
+res.status(200).json({ weatherData: cityData });
+} else {
+res.status(404).send({
+message: "Weather data not found for the given city",
+});
+}
+} catch (error) {
+res.status(500).json({
+message: "Internal server error",
+error: error.message,
+});
+}
 
-  try {
-    // TODO: Implement logic to retrieve weather data by city name
-    // Example response when data is found:
-    // res.status(200).json({ weatherData });
-    // Example response when data is not found:
-    // res.status(404).json({ message: 'Weather data not found for the given city' });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal server error', error: error.message });
-  }
+  
+  // const cityName = req.params.cityName;
+  // try {
+  //   // TODO: Implement logic to retrieve weather data by city name
+  //   // Example response when data is found:
+  //   // res.status(200).json({ weatherData });
+  //   // Example response when data is not found:
+  //   // res.status(404).json({ message: 'Weather data not found for the given city' });
+  //   const weatherData = JSON.parse(fs.readFileSync(`./data/data.json`))
+  //   const dataByCityName = weatherData.find((el)=> el.cityName.toLowerCase().trim() === cityName.toLowerCase().trim());
+  //   if(dataByCityName){
+  //     return res.status(200).json({"weatherData": dataByCityName});
+  //   }
+  //   return res.status(404).json({ message: 'Weather data not found for the given city' });
+  // } catch (error) {
+  //   res
+  //     .status(500)
+  //     .json({ message: 'Internal server error', error: error.message });
+  // }
 };
 
 const getWeatherByZipCode = async (req, res) => {
@@ -25,6 +53,12 @@ const getWeatherByZipCode = async (req, res) => {
     // res.status(200).json({ weatherData });
     // Example response when data is not found:
     // res.status(404).json({ message: 'Weather data not found for the given zip code' });
+    const weatherData = JSON.parse(fs.readFileSync(`./data/data.json`))
+    const dataByCityName = weatherData.find((el)=> el.zipCode === zipCode);
+    if(dataByCityName){
+      return res.status(200).json({"weatherData": dataByCityName});
+    }
+    return res.status(404).json({ message: 'Weather data not found for the given zip code' });
   } catch (error) {
     res
       .status(500)
@@ -40,6 +74,11 @@ const postWeatherAlert = async (req, res) => {
     // TODO: Implement logic to post weather alert
     // Example response when alert is posted successfully:
     // res.status(201).json({ message: 'Weather alert posted successfully', alert: newAlert });
+    if(!cityName || !humidity || !weatherDescription || !temperature || !zipCode){
+      return res.status(500).json({message: "Internal server error"});
+    }
+    const alert =  await WeatherData.create(req.body);
+    return res.status(201).json({ "message": "Weather alert posted successfully", alert});
   } catch (error) {
     res
       .status(500)
--- models/weatherDataModel.js
+++ models/weatherDataModel.js
@@ -6,6 +6,31 @@ const weatherDataSchema = new mongoose.Schema({
   // Zip code (Type: String, Required: true)
   // Humidity percentage (Type: Number, Required: true)
   // Weather description (Type: String, Required: true)
+  cityName:{
+    type: String,
+    required: true,
+    description: 'The name of the city for which the weather data is recorded.'
+  },
+  temperature :{
+    type: Number,
+    required: true,
+    description:'The temperature at the location.'
+  },
+  zipCode :{
+    type: String,
+    required: true,
+    description:'The zip code of the location.'
+  },
+  humidity:{
+    type: Number,
+    required: true,
+    description:'The humidity at the location.'
+  },
+  weatherDescription:{
+    type: String,
+    required: true,
+    description:'A description of the weather at the location.'
+  }
 });
 
 const WeatherData = mongoose.model('WeatherData', weatherDataSchema);
